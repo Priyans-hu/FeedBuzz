1ae5f416d5b234bb74c6fdbd0717db56ca03d0df
priyans-hu <gargpriyanshu2004@gmail.com>
2023-10-25 11:06:29 +0530

    updated code

--- Scripts/checkLogin.js
+++ Scripts/checkLogin.js
@@ -1,9 +1,9 @@
 const urlParams = new URLSearchParams(window.location.search);
-    const loggedIn = urlParams.get("loggedIn");
+const loggedIn = urlParams.get("loggedIn");
 
-    if (loggedIn === "true") {
-        document.getElementById('headerAddPostBtn').style.display = 'initial';
-        document.getElementById('logInBtn').style.display = 'none';
-        const decodedUsername = decodeURIComponent(urlParams.get('username'));
-        console.log("Logged in as " + decodedUsername);
-    }
+if (loggedIn === "true") {
+    document.getElementById('headerAddPostBtn').style.display = 'initial';
+    document.getElementById('logInBtn').style.display = 'none';
+    const decodedUsername = decodeURIComponent(urlParams.get('username'));
+    console.log("Logged in as " + decodedUsername);
+}
