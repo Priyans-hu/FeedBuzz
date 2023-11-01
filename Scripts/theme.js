46a490e1040d35540902f3edd4609dabf3c0b6ab
priyans-hu <gargpriyanshu2004@gmail.com>
2023-11-01 18:35:34 +0530

    added light and dark theme button

--- Scripts/theme.js
+++ Scripts/theme.js
@@ -1,10 +1,28 @@
 let allIElements = document.querySelectorAll("#menuBtnCont button i");
 let postInfoElements = document.querySelectorAll('.postInfo');
+const themeBtn = document.getElementById("changeThemeBtn");
+const themeIcon = document.getElementById("themeIcon");
+
+themeBtn.addEventListener("click", () => {
+    const theme = urlParams.get("theme");
+    
+    if (theme != "light") {
+        let currentUrl = new URL(window.location);
+        currentUrl.searchParams.set('theme', 'light');
+        window.location.href = currentUrl.href;
+    } else {
+        let currentUrl = new URL(window.location);
+        currentUrl.searchParams.delete('theme', 'light');
+        window.location.href = currentUrl.href;
+    }
+})
+
 
 function applyTheme() {
     const theme = urlParams.get("theme");
 
     if (theme === "light") {
+        themeIcon.classList.add('fa-solid', 'fa-moon');
         addLightTheme();
     }
 }
